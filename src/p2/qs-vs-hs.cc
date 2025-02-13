9e0c043a800a4fe89836236f031d74ac19a3b3d7
n4th0 <nathanmoyses@gmail.com>
2025-02-13 16:34:52 +0100

    added practice 2

--- src/p2/qs-vs-hs.cc
+++ src/p2/qs-vs-hs.cc
@@ -3,11 +3,14 @@ ADA. 2024-25
 Practice 2: "Empirical analysis by means of program-steps account of two sorting
 algorithms: Middle-Quicksort and Heapsort."
 */
+#include <cstdlib>
 #include <iostream>
 #include <math.h>
 #include <unistd.h>
 
 using namespace std;
+
+size_t PASOS;
 
 //--------------------------------------------------------------
 // Middle-Quicksort:
@@ -26,17 +29,23 @@ void middle_QuickSort(int *v, long left, long right) {
     i = left;
     j = right;
     pivot = v[(i + j) / 2];
+    // PASOS++;
     // pivot based partitioning:
     do {
-      while (v[i] < pivot)
+      while (v[i] < pivot) {
+        PASOS++;
         i++;
-      while (v[j] > pivot)
+      }
+      while (v[j] > pivot) {
+        PASOS++;
         j--;
+      }
       if (i <= j) {
         swap(v[i], v[j]);
         i++;
         j--;
       }
+      PASOS++;
     } while (i <= j);
     // Repeat for each non-empty subarray:
     if (left < j)
@@ -64,6 +73,7 @@ void sink(int *v, size_t n, size_t i)
   size_t l, r; // indices of left and right childs
 
   do {
+    PASOS++;
     largest = i;   // Initialize largest as root
     l = 2 * i + 1; // left = 2*i + 1
     r = 2 * i + 2; // right = 2*i + 2
@@ -94,6 +104,7 @@ void heapSort(int *v, size_t n) {
   // Starting from the last non-leaf node (right to left), sink each element to
   // construct the heap.
   for (size_t i = n / 2 - 1; true; i--) {
+    // PASOS++;
     sink(v, n, i);
     if (i == 0)
       break; // As size_t is an unsigned type
@@ -103,6 +114,7 @@ void heapSort(int *v, size_t n) {
   // Repeatedly swap the root (largest element) with the last element and
   // rebuild the heap.
   for (size_t i = n - 1; i > 0; i--) {
+    // PASOS++;
     // Move the root (largest element) to the end by swapping it with the last
     // element.
     swap(v[0], v[i]);
@@ -115,4 +127,104 @@ void heapSort(int *v, size_t n) {
   }
 }
 
-int main() {}
+void cabecera() {
+  cout << "# QUICKSORT VERSUS HEAPSORT.                                        "
+          "         "
+       << endl;
+  cout << "# Average processing Msteps (millions of program steps)             "
+          "         "
+       << endl;
+  cout << "# Number of samples (arrays of integer): 30                         "
+          "         "
+       << endl;
+  cout << "# RANDOM ARRAYS SORTED ARRAYS REVERSE SORTED ARRAYS                 "
+          "         "
+       << endl;
+  cout << "# ------------------- ------------------- ---------------------     "
+          "         "
+       << endl;
+  cout << "# Size QuickSort HeapSort QuickSort HeapSort QuickSort HeapSort     "
+          "         "
+       << endl;
+  cout << "#==================================================================="
+          "========="
+       << endl;
+}
+
+int main() {
+  cabecera();
+
+  srand(getpid());
+
+  for (int exp = 15; exp <= 20; exp++) {
+    float average_random = 0;
+    float average2_random = 0;
+    float average_sorted = 0;
+    float average2_sorted = 0;
+    float average_reverse = 0;
+    float average2_reverse = 0;
+
+    size_t size = size_t(pow(2, exp));
+    cout << size << "\t" << std::flush;
+    int *v;
+    int *v2;
+    v = new int[size];
+    if (!v) {
+      cerr << "Error, not enough memory!" << endl;
+      exit(EXIT_FAILURE);
+    }
+
+    v2 = new int[size];
+    if (!v2) {
+      cerr << "Error, not enough memory!" << endl;
+      exit(EXIT_FAILURE);
+    }
+
+    for (size_t i = 0; i < 30; i++) {
+
+      for (size_t j = 0; j < size; j++) {
+        int aux = rand();
+        v[j] = aux;
+        v2[j] = aux;
+      }
+
+      PASOS = 0;
+      middle_QuickSort(v, 0, size);
+      average_random += (float)(PASOS) / 1000000;
+
+      PASOS = 0;
+      heapSort(v, size);
+      average2_random += (float)(PASOS) / 1000000;
+    }
+
+    for (size_t j = 0; j < size; j++)
+      v[j] = v2[j] = j;
+
+    PASOS = 0;
+    middle_QuickSort(v, 0, size);
+    average_sorted += (float)(PASOS) / 1000000;
+
+    PASOS = 0;
+    heapSort(v, size);
+    average2_sorted += (float)(PASOS) / 1000000;
+
+    for (size_t j = size; j > 0; j--)
+      v[j] = v2[j] = j;
+
+    PASOS = 0;
+    middle_QuickSort(v, 0, size);
+    average_reverse += (float)(PASOS) / 1000000;
+
+    PASOS = 0;
+    heapSort(v, size);
+    average2_reverse += (float)(PASOS) / 1000000;
+
+    cout << average_random / 30 << ' ' << average2_random / 30 << ' '
+         << average_sorted << ' ' << average2_sorted << ' ' << average_reverse
+         << ' ' << average2_reverse;
+    cout << endl;
+
+    delete[] v;
+    delete[] v2;
+  }
+}
