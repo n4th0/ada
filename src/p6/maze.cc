d97300a8bf65aee9b56f626a39f86e19c37363c0
n4th0 <nathanmoyses@gmail.com>
2025-03-15 12:19:03 +0100

    added final version for p6 ada

--- src/p6/maze.cc
+++ src/p6/maze.cc
@@ -30,43 +30,6 @@ int min(int a, int b, int c) {
 
 vector<Coord> parsing;
 
-/*
-int maze_memo(int final_i, int final_j, int pos_i, int pos_j, int ac,
-              const vector<vector<int>> &matrix, vector<vector<int>> &memo) {
-
-  if (memo[pos_i][pos_j] != CENTINELA) {
-    return memo[pos_i][pos_j];
-  }
-
-  if (matrix[pos_i][pos_j] == 0) {
-    return 0;
-  }
-
-  ac++;
-  if (final_i == pos_i && final_j == pos_j) {
-    return ac;
-  }
-
-  int a = numeric_limits<int>::max(), b = numeric_limits<int>::max(),
-      c = numeric_limits<int>::max();
-  if (pos_i + 1 <= final_i && matrix[pos_i + 1][pos_j] == 1) {
-    a = maze_memo(final_i, final_j, pos_i + 1, pos_j, ac, matrix, memo);
-  }
-
-  if (pos_j + 1 <= final_j && matrix[pos_i][pos_j + 1] == 1) {
-    b = maze_memo(final_i, final_j, pos_i, pos_j + 1, ac, matrix, memo);
-  }
-
-  if (pos_i + 1 <= final_i && pos_j + 1 <= final_j &&
-      matrix[pos_i + 1][pos_j + 1] == 1) {
-    c = maze_memo(final_i, final_j, pos_i + 1, pos_j + 1, ac, matrix, memo);
-  }
-
-  memo[pos_i][pos_j] = ac;
-  return min(a, b, c);
-}
-*/
-
 ///
 /// maze_naive O(3**n) -> n = m*l
 /// final_i -> 0
@@ -111,8 +74,8 @@ int maze_naive(int final_i, int final_j, int pos_i, int pos_j,
   return aux;
 }
 
-int maze_naive2(int final_i, int final_j, int pos_i, int pos_j,
-                const vector<vector<int>> &matrix, vector<vector<int>> &memo) {
+int maze_memo(int final_i, int final_j, int pos_i, int pos_j,
+              const vector<vector<int>> &matrix, vector<vector<int>> &memo) {
 
   if (matrix[pos_i][pos_j] == 0) {
     memo[pos_i][pos_j] = numeric_limits<int>::max();
@@ -127,19 +90,19 @@ int maze_naive2(int final_i, int final_j, int pos_i, int pos_j,
   }
   int a, b, c;
   if (pos_i - 1 >= final_i) {
-    a = maze_naive2(final_i, final_j, pos_i - 1, pos_j, matrix, memo);
+    a = maze_memo(final_i, final_j, pos_i - 1, pos_j, matrix, memo);
   } else {
     a = numeric_limits<int>::max();
   }
 
   if (pos_j - 1 >= final_j) {
-    b = maze_naive2(final_i, final_j, pos_i, pos_j - 1, matrix, memo);
+    b = maze_memo(final_i, final_j, pos_i, pos_j - 1, matrix, memo);
   } else {
     b = numeric_limits<int>::max();
   }
 
   if (pos_i - 1 >= final_i && pos_j - 1 >= final_j) {
-    c = maze_naive2(final_i, final_j, pos_i - 1, pos_j - 1, matrix, memo);
+    c = maze_memo(final_i, final_j, pos_i - 1, pos_j - 1, matrix, memo);
   } else {
     c = numeric_limits<int>::max();
   }
@@ -172,6 +135,7 @@ int main(int argc, char **argv) {
     print_usage();
     exit(-1);
   }
+
   string filename;
   bool p2D_flag = false;
   bool t_flag = false;
@@ -221,51 +185,37 @@ int main(int argc, char **argv) {
   if (ignoreNaive_flag) {
   }
 
-  for (size_t i = 0; i < matrix.size(); i++) {
-    for (size_t j = 0; j < matrix[i].size(); j++) {
-      cout << matrix[i][j];
-    }
-    cout << endl;
+  vector<vector<int>> memo(rows, vector<int>(cols, CENTINELA));
+
+  if (!ignoreNaive_flag) {
+    cout << maze_naive(0, 0, matrix.size() - 1, matrix[0].size() - 1, matrix);
+  } else {
+    cout << "-";
   }
-  vector<vector<int>> memo(rows, vector<int>(cols));
 
-  for (int i = 0; i < rows; i++)
-    for (int j = 0; j < cols; j++)
-      memo[i][j] = CENTINELA;
+  cout << " ";
+  cout << maze_memo(0, 0, matrix.size() - 1, matrix[0].size() - 1, matrix, memo)
+       << " ? ?" << endl;
 
-  cout << "sol: "
-       << maze_naive2(0, 0, matrix.size() - 1, matrix[0].size() - 1, matrix,
-                      memo)
-       << endl;
+  if (p2D_flag) {
+    cout << "?" << endl;
+  }
 
-  // for (int i = 0; i < rows; i++) { // la clave para el parsing
-  //   cout << " ";
-  //   for (int j = 0; j < cols; j++) {
-  //     if (memo[i][j] == -1) {
-  //       cout << " - ";
-  //     } else if (memo[i][j] == numeric_limits<int>::max()) {
-  //       cout << " X ";
-  //     } else {
-  //       cout << " " << memo[i][j] << " ";
-  //     }
-  //   }
-  //   cout << endl;
-  // }
+  if (t_flag) {
 
-  // cout << parsing.size() << endl;
-  // for (size_t i = 0; i < parsing.size(); i++) {
-  //   // matrix[parsing[i].i][parsing[i].j] = 3;
-  //   cout << parsing[i].i << " " << parsing[i].j << endl;
-  // }
-
-  //   for (unsigned i = 0; i < matrix.size(); i++) {
-  //     for (unsigned j = 0; j < matrix[i].size(); j++) {
-  //       if (matrix[i][j] == 3) {
-  //         cout << '*';
-  //       } else {
-  //         cout << matrix[i][j];
-  //       }
-  //     }
-  //     cout << endl;
-  //   }
+    // for (int i = 0; i < rows; i++) { // la clave para el parsing
+    //   cout << " ";
+    //   for (int j = 0; j < cols; j++) {
+    //     if (memo[i][j] == -1) {
+    //       cout << " - ";
+    //     } else if (memo[i][j] == numeric_limits<int>::max()) {
+    //       cout << " X ";
+    //     } else {
+    //       cout << " " << memo[i][j] << " ";
+    //     }
+    //   }
+    //   cout << endl;
+    // }
+    cout << "?" << endl;
+  }
 }
