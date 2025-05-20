c4b1ff69f4790ad591c14e20d4a0bae602eb5d97
n4th0 <nathanmoyses@gmail.com>
2025-05-20 10:57:17 +0000

    added version 2

--- src/pFinal/script.sh
+++ src/pFinal/script.sh
@@ -13,6 +13,30 @@ for ((i = 0; i < 14; i++)); do
     test=$test"$i-bb.maze"
     solu=$solu"$i-bb.maze.sol_bb"
   fi
-  echo $solu
+
+  echo "------------------------------"
+  echo "test "$i
+  ./maze_bb -f $test
+  echo "----------------"
+  cat $solu | head -n 3
 
 done
+
+execute() {
+  echo "------------------------------"
+  echo "test "$1
+  ./maze_bb -f "tests/"$1"-bb.maze"
+  echo "----------------"
+  cat "tests/"$1"-bb.maze.sol_bb" | head -n 3
+}
+
+execute "200"
+execute "300"
+execute "500"
+execute "700"
+execute "900"
+execute "k01"
+execute "k02"
+execute "k03"
+execute "k05"
+execute "k10"
